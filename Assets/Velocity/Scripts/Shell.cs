a2c0552d5c8303770fb5bd37119a92a71e50c5f0
Carlos Guedes <carlosguedesss17@gmail.com>
2023-10-29 02:04:36 +0000

    a52198_Acceleration Part 2

--- Assets/Velocity/Scripts/Shell.cs
+++ Assets/Velocity/Scripts/Shell.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System.Security.AccessControl;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+    [SerializeField] float speed = 5f;
+
+    float mass = 10f;
+    float force = 1000f;
+    float acceleration;
 
     void OnCollisionEnter(Collision col)
     {
@@ -25,6 +31,9 @@ public class Shell : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        acceleration = force / mass;
+        speed += acceleration * Time.deltaTime;
 
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
     }
 }
