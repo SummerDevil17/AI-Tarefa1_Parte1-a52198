f43ad808b196b620b85114a893cec8d257b361c7
Carlos Guedes <carlosguedesss17@gmail.com>
2023-10-29 01:57:45 +0100

    a52198_Time

---
+++ Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStartOffset = 0f;
+    bool gotStartTime = false;
+
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+
+        this.transform.position = new Vector3(transform.position.x, transform.position.y,
+                                            Time.realtimeSinceStartup - timeStartOffset);
+    }
+}
