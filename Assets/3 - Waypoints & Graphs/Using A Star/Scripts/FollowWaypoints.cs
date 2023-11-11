dd8c3afb8285262c15d344dba072c6f0986d983c
Carlos Guedes <carlosguedesss17@gmail.com>
2023-11-11 17:14:17 +0000

    a52198_Traversing a Path

--- Assets/3 - Waypoints & Graphs/Using A Star/Scripts/FollowWaypoints.cs
+++ Assets/3 - Waypoints & Graphs/Using A Star/Scripts/FollowWaypoints.cs
@@ -9,11 +9,11 @@ public class FollowWaypoints : MonoBehaviour
     [Header("Movement Variables")]
     [SerializeField] float speed = 10f;
     [SerializeField] float rotationSpeed = 3f;
-    [SerializeField] float accuracy = 1f;
+    [SerializeField] float accuracy = 5f;
 
     GameObject[] waypoints;
     GameObject currentNode;
-    int currentWaypoint = 0;
+    int currentWaypointIndex = 0;
     Graph graph;
 
     Transform goal;
@@ -22,23 +22,54 @@ public class FollowWaypoints : MonoBehaviour
     {
         waypoints = WPManagerReference.waypoints;
         graph = WPManagerReference.graph;
-        currentNode = waypoints[currentWaypoint];
+        currentNode = waypoints[currentWaypointIndex];
+
+        Invoke("GoToRuin", 2f);
     }
 
-    void Update()
+    void LateUpdate()
     {
+        if (graph.pathList.Count == 0 || currentWaypointIndex == graph.pathList.Count) { return; }
 
+        if (Vector3.Distance(transform.position, graph.pathList[currentWaypointIndex].GetObjID().transform.position) < accuracy)
+        {
+            currentNode = graph.pathList[currentWaypointIndex].GetObjID();
+            currentWaypointIndex++;
+        }
+        if (currentWaypointIndex < graph.pathList.Count)
+        {
+            goal = graph.pathList[currentWaypointIndex].GetObjID().transform;
+
+            Vector3 goalToLookAt = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+            Vector3 goalDirection = goalToLookAt - transform.position;
+
+            transform.rotation =
+            Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goalDirection), rotationSpeed * Time.deltaTime);
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 
     public void GoToHelipad()
     {
         graph.AStar(currentNode, waypoints[waypoints.Length - 1]);
-        currentWaypoint = 0;
+        currentWaypointIndex = 0;
     }
 
     public void GoToRuin()
     {
         graph.AStar(currentNode, waypoints[4]);
-        currentWaypoint = 0;
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToMill()
+    {
+        graph.AStar(currentNode, waypoints[11]);
+        currentWaypointIndex = 0;
+    }
+
+    public void GoToSkeleton()
+    {
+        graph.AStar(currentNode, waypoints[0]);
+        currentWaypointIndex = 0;
     }
 }
