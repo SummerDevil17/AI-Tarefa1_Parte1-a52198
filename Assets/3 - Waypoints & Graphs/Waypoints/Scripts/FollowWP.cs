e64c809b63353a18437a3df1b7bb51b3ea6ff581
Carlos Guedes <carlosguedesss17@gmail.com>
2023-11-11 11:26:44 +0000

    a52198_Following a Tracker

--- Assets/3 - Waypoints & Graphs/Waypoints/Scripts/FollowWP.cs
+++ Assets/3 - Waypoints & Graphs/Waypoints/Scripts/FollowWP.cs
@@ -8,26 +8,43 @@ public class FollowWP : MonoBehaviour
 
     [SerializeField] float speed = 10f;
     [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] float trackerLookAhead = 10f;
 
+    GameObject circuitTracker;
     int currentWaypointIndex = 0;
 
     void Start()
     {
+        circuitTracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(circuitTracker.GetComponent<Collider>());
+        circuitTracker.GetComponent<MeshRenderer>().enabled = false;
 
+        circuitTracker.transform.position = this.transform.position;
+        circuitTracker.transform.rotation = this.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 3f)
+        ProgressTracker();
+
+        Quaternion lookAtRotation = Quaternion.LookRotation(circuitTracker.transform.position - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRotation, rotationSpeed * Time.deltaTime);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(circuitTracker.transform.position, transform.position) > trackerLookAhead) { return; }
+
+        if (Vector3.Distance(circuitTracker.transform.position, waypoints[currentWaypointIndex].position) < 3f)
         { currentWaypointIndex++; }
 
         if (currentWaypointIndex >= waypoints.Length)
         { currentWaypointIndex = 0; }
 
-        Quaternion lookAtRotation = Quaternion.LookRotation(waypoints[currentWaypointIndex].position - transform.position);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRotation, rotationSpeed * Time.deltaTime);
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        circuitTracker.transform.LookAt(waypoints[currentWaypointIndex].position);
+        circuitTracker.transform.Translate(0, 0, (speed + 50f) * Time.deltaTime);
     }
 }
