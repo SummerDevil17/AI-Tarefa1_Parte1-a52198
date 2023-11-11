d43da2c2501425c021179efa3b6af11dee8cfc8d
Carlos Guedes <carlosguedesss17@gmail.com>
2023-11-11 12:51:38 +0000

    a52198_A Simple Graph API Part 2

--- Assets/3 - Waypoints & Graphs/Using A Star/Scripts/Graphs API/Graph.cs
+++ Assets/3 - Waypoints & Graphs/Using A Star/Scripts/Graphs API/Graph.cs
@@ -47,5 +47,83 @@ public class Graph
         return null;
     }
 
+    public bool AStar(GameObject startObj, GameObject endObj)
+    {
+        Node start = FindNode(startObj);
+        Node end = FindNode(endObj);
 
+        if (start == null || end == null) { return false; }
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+
+        float tentativeGScore = 0;
+        bool tentativeIsBetter;
+
+        start.g = 0;
+        start.h = DistanceH(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+
+        while (open.Count > 0)
+        {
+            int i = LowestF(open);
+            Node currentNode = open[i];
+
+            if (currentNode.GetObjID() == endObj)
+            {//ReconstructPath(start,end); 
+                return true;
+            }
+            open.RemoveAt(i);
+            closed.Add(currentNode);
+
+            Node neighbourNode;
+            foreach (Edge e in currentNode.edgeList)
+            {
+                neighbourNode = e.endNode;
+
+                if (closed.IndexOf(neighbourNode) > -1) { continue; }
+
+                tentativeGScore = currentNode.g + DistanceH(currentNode, neighbourNode);
+                if (open.IndexOf(neighbourNode) == -1)
+                {
+                    open.Add(neighbourNode);
+                    tentativeIsBetter = true;
+                }
+                else if (tentativeGScore < neighbourNode.g) { tentativeIsBetter = true; }
+                else { tentativeIsBetter = false; }
+
+                if (tentativeIsBetter)
+                {
+                    neighbourNode.cameFrom = currentNode;
+                    neighbourNode.g = tentativeGScore;
+                    neighbourNode.h = DistanceH(currentNode, end);
+                    neighbourNode.f = neighbourNode.g + neighbourNode.h;
+                }
+            }
+        }
+        return false;
+    }
+
+    private float DistanceH(Node a, Node b)
+    {
+        return (Vector3.SqrMagnitude(a.GetObjID().transform.position - b.GetObjID().transform.position));
+    }
+
+    private int LowestF(List<Node> nodes)
+    {
+        int count = 0;
+        int iteratorCount = 0;
+
+        float lowestF = nodes[0].f;
+
+        for (int i = 1; i < nodes.Count; i++)
+        {
+            if (nodes[i].f < lowestF) { lowestF = nodes[i].f; iteratorCount = count; }
+
+            count++;
+        }
+        return iteratorCount;
+    }
 }
